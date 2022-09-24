d3b4567a2075ff039d3feadc6f1b41ffcc8dc525
Nidhin-mohan <nidhinmohannidhin>
2022-09-24 21:08:52 +0530

    Introduction to DOM

---
+++ 04DOM/proj1/script.js
@@ -0,0 +1,20 @@
+var counter = document.getElementById("counter");
+var followers = document.getElementById("followers");
+let count = 1;
+
+setInterval( () => {
+   if (count< 1000)
+   {
+     count++;
+     counter.innerText = count;
+   }
+
+}, 1);
+
+setTimeout( () => {
+    followers.innerText = "Followers in Instagram!";
+   
+}, 4000);
+
+
+
---
+++ 05notAdvanced/01objects.js
@@ -0,0 +1,26 @@
+var User = function (firstName, courseCount){
+    this.firstName = firstName;
+    this.courseCount = courseCount;
+    this.getCourseCount = function () {
+        console.log(`Course count is : ${this.courseCount}`);
+
+    };
+};
+
+User.prototype.getFirstname = function (){
+    console.log(`Your first Name is: ${this.firstName}`);
+};
+
+var hitesh = new User("hitesh", 2);
+
+// console.log(hitesh);
+
+var sam = new User ("Sam",1);
+// console.log(sam);
+
+hitesh.getCourseCount();
+hitesh.getFirstname();
+ 
+
+sam.getCourseCount();
+sam.getFirstname();
