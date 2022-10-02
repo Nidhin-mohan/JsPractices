2035f08bdbca45a5ecadae9a865b4e3ebb2ea3db
Nidhin-mohan <nidhinmohannidhin>
2022-10-02 16:48:05 +0530

    counter project added

---
+++ zExtra/counter project/script.js
@@ -0,0 +1,65 @@
+let count = 0;
+
+let value = document.getElementById("value");
+
+let btns = document.querySelectorAll(".btn")
+let check ;
+
+btns.forEach((items) => {
+  items.addEventListener("click", (val) => {
+let style = val.currentTarget.classList;
+ 
+if(style.contains("increase")){
+  count++;
+}
+  else if (style.contains("decrease")) {
+    count--;
+  } else if (style.contains("reset")) {
+    count = 0;
+  }
+
+ if (count > 0) {
+     value.style.color = "Blue";
+   } else {
+     value.style.color = "Red";
+    }
+
+
+value.textContent = count;
+
+  })
+})
+
+
+
+
+
+// let count = 0;
+
+// let value = document.querySelector("#value");
+// // console.log(value);
+// let btns = document.querySelectorAll(".btn");
+// // console.log(btns);
+
+// btns.forEach(function (item) {
+//   //   console.log(item);
+//   item.addEventListener("click", function (val) {
+//     // console.log(val.currentTarget);
+//     // console.log(val.currentTarget.classList);
+//     let style = val.currentTarget.classList;
+//     // console.log(style);
+//     if (style.contains("increase")) {
+//       count++;
+//     } else if (style.contains("decrease")) {
+//       count--;
+//     } else if (style.contains("reset")) {
+//       count = 0;
+//     }
+//     if (count > 0) {
+//       value.style.color = "Blue";
+//     } else {
+//       value.style.color = "Red";
+//     }
+//     value.textContent = count;
+//   });
+// });
